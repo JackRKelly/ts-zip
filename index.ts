f7bca1b1b5eb7bc1c42231324a2ab4617357babc
unknown <kcjackkelly@gmail.com>
2020-12-11 10:01:42 -0600

    Added buffer to string function

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import * as fs from "fs";
 
-console.log("test");
-
 let zipPath = "./test.zip";
 
+const bufferToString = (buffer: Buffer) => {
+  return buffer.toString("hex").match(/../g).join(" ");
+};
+
 fs.readFile(zipPath, (err, data) => {
+  if (err) console.error(err);
   console.log(data);
+  console.log(data.length);
+  console.log(data.toString("hex").match(/../g).join(" "));
+
+  let signature = data.slice(0, 4);
+  console.log(bufferToString(signature));
 });
